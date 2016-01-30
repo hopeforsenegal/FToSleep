f3137f7da738d6664fbd508ea171149b53b882d6
Kamau Vassall <kamau.vassall@gmail.com>
2016-01-30 17:36:43 -0600

    Adding a timer count down to when the game should exit

--- FToSleep/Assets/Scripts/DoorsManager.cs
+++ FToSleep/Assets/Scripts/DoorsManager.cs
@@ -3,13 +3,83 @@ using System.Collections;
 
 public class DoorsManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+	// Singleton
+	public static DoorsManager Instance {
+		get {
+			return instance;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	private static DoorsManager instance;
+
+	private float deltaTime = 0.0f;
+	private float trackingTime = 0.0f;
+	private int countDownEndSeconds = 0;
+	public int endCountdownTime = 30;
+	public bool matchStarted;
+	public bool startGameOnLaunch;
+
+	protected void Awake ()
+	{
+		if (instance == null) {
+			instance = this;
+		}
+	}
+
+	protected void Start ()
+	{
+		trackingTime = Time.time;
+		deltaTime = Time.time;
+
+		RestartCountdown ();
+		if (startGameOnLaunch) {
+			StartGame ();
+		}
+	}
+
+	protected void Update ()
+	{
+		if (IsMatchStarted ()) {
+			// Change the remaining time
+			if (Time.time - deltaTime >= 1.0f) {
+				deltaTime = Time.time;
+
+				countDownEndSeconds--;
+				RemainingTimeText.SetTimeRemaining (countDownEndSeconds);
+			}
+
+			RemainingTimeText.Show (true);
+			trackingTime += Time.deltaTime;
+
+			// If the time has run down 
+			if (countDownEndSeconds <= 0) {
+				EndGame ();
+			}
+		}
+	}
+
+	public void StartGame ()
+	{
+		Debug.Log ("Start Doors Game");
+
+		matchStarted = true;
+	}
+
+	public bool IsMatchStarted ()
+	{
+		return matchStarted;
+	}
+
+	public void EndGame ()
+	{
+		Debug.Log ("End Doors Game");
+
+		matchStarted = false;
+	}
+
+	public void RestartCountdown ()
+	{
+		countDownEndSeconds = endCountdownTime;
+		RemainingTimeText.SetTimeRemaining (countDownEndSeconds);
 	}
 }
