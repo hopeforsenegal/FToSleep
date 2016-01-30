d3859467091a95d9c379b5f1ad5fd01c74637517
Kamau Vassall <kamau.vassall@gmail.com>
2016-01-30 01:19:27 -0600

    Getting tech for a movable paddle

--- FToSleep/Assets/Scripts/Paddle.cs
+++ FToSleep/Assets/Scripts/Paddle.cs
@@ -7,12 +7,15 @@ using System.Collections.Generic;
 public class Paddle : MonoBehaviour
 {
 	private float maxMoveSpeed;
+	private float moveDeadZone;
 	private float moveAcceleration;
 	private float deceleration;
 	private float slowMoveSpeedRatio;
 	private float fastMoveSpeedRatio;
 
+	private bool moved;
 	private float currentMoveSpeed;
+
 
 	protected void Awake ()
 	{
@@ -24,14 +27,29 @@ public class Paddle : MonoBehaviour
 
 	protected void Update ()
 	{
-		rigidbody2D.velolcity = new Vector2 (currentMoveSpeed, 0);
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (currentMoveSpeed, 0);
+
+		if (!moved) {
+			if (Mathf.Abs (currentMoveSpeed) < moveDeadZone) {
+				currentMoveSpeed = 0;
+			} else if (currentMoveSpeed < 0) {
+				currentMoveSpeed += deceleration * Time.deltaTime;
+			} else {
+				currentMoveSpeed -= deceleration * Time.deltaTime;
+			}
+		}
 	}
 
-	public void MoveRight ()
+	public void MoveLeft()
 	{
+			moved = true;
+			currentMoveSpeed -= moveAcceleration * Time.deltaTime;
+			currentMoveSpeed = Mathf.Max(-1 * maxMoveSpeed, currentMoveSpeed);
 	}
 
-	public void MoveLeft ()
-	{
+	public void MoveRight( ) {
+			moved = true;
+			currentMoveSpeed += moveAcceleration * Time.deltaTime;
+			currentMoveSpeed = Mathf.Min(maxMoveSpeed, currentMoveSpeed);
 	}
 }
