8243e87d90dbbf29e325278cce5e6432ff9dabdb
Kamau Vassall <kamau.vassall@gmail.com>
2016-01-30 01:12:24 -0600

    What is going on?

---
+++ FToSleep/Assets/Scripts/Paddle.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Paddle : MonoBehaviour
+{
+	private float maxMoveSpeed;
+	private float moveAcceleration;
+	private float deceleration;
+	private float slowMoveSpeedRatio;
+	private float fastMoveSpeedRatio;
+
+	private float currentMoveSpeed;
+
+	protected void Awake ()
+	{
+	}
+
+	protected void Start ()
+	{
+	}
+
+	protected void Update ()
+	{
+		rigidbody2D.velolcity = new Vector2 (currentMoveSpeed, 0);
+	}
+
+	public void MoveRight ()
+	{
+	}
+
+	public void MoveLeft ()
+	{
+	}
+}
--- FToSleep/Assets/Scripts/PlayerInput.cs
+++ FToSleep/Assets/Scripts/PlayerInput.cs
@@ -1,18 +1,28 @@
 using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Paddle))]
 public class PlayerInput : MonoBehaviour
 {
+	private Paddle paddle;
+
+	protected void Awake ()
+	{
+		paddle = GetComponent<Paddle> ();
+	}
+
 	protected void Update ()
 	{
 		if (Input.GetButtonDown ("Up")) {
 			Debug.Log ("UP was pressed", gameObject);
 		} else if (Input.GetButtonDown ("Right")) {
 			Debug.Log ("RIGHT was pressed", gameObject);
+			paddle.MoveRight ();
 		} else if (Input.GetButtonDown ("Down")) {
 			Debug.Log ("DOWN was pressed", gameObject);
 		} else if (Input.GetButtonDown ("Left")) {
 			Debug.Log ("LEFT was pressed", gameObject);
+			paddle.MoveLeft ();
 		}
 	}
 }
