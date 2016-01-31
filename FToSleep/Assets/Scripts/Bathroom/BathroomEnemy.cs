1e8bc161c6983c8498ddbcd478a4f35f35737d2d
Kamau Vassall <kamau.vassall@gmail.com>
2016-01-31 03:56:32 -0600

    Can no longer push the bad guys

--- FToSleep/Assets/Scripts/Bathroom/BathroomEnemy.cs
+++ FToSleep/Assets/Scripts/Bathroom/BathroomEnemy.cs
@@ -7,15 +7,18 @@ using System.Collections.Generic;
 public class BathroomEnemy : MonoBehaviour
 {
 	private Knight2D mainPlayer;
-	protected void Awake(){
+
+	protected void Awake ()
+	{
 		GameObject tempObject;
-		tempObject = GameObject.Find ("Canvas");
+		tempObject = GameObject.Find ("Knight");
 		if (tempObject != null) {
 			mainPlayer = tempObject.GetComponent<Knight2D> ();
 		}
 	}
 
-	void OnCollisionEnter2D(Collision2D coll) {
+	void OnCollisionEnter2D (Collision2D coll)
+	{
 		Debug.Log ("Did you swing at me?");
 		if (coll.gameObject.name == "Knight") {
 			Debug.Log ("Et tu, Brute?");
@@ -23,15 +26,22 @@ public class BathroomEnemy : MonoBehaviour
 				mainPlayer.GotTouched ();
 			}
 		}
+		if (coll.gameObject.name == "Sword") {
+			Debug.Log ("Sword");
+			gameObject.SetActive (false);
+		}
 	}
 
-    void OnTriggerEnter2D(Collider2D otherObject)
-    {
-        Debug.Log("Got hit by sword?");
-        if(otherObject.name == "AttackTrigger")
-        {
-            Debug.Log("Attack Trigger");
-            gameObject.SetActive(false);
-        }
-    }
+	void OnTriggerEnter2D (Collider2D otherObject)
+	{
+		Debug.Log ("Got hit by sword?");
+		if (otherObject.name == "Sword") {
+			Debug.Log ("Attack Trigger");
+			gameObject.SetActive (false);
+		}
+		if (otherObject.name == "AttackTrigger") {
+			Debug.Log ("Sword");
+			gameObject.SetActive (false);
+		}
+	}
 }
