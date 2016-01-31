c0483a2760284ad2b1e451baea782c1cf1822622
Ken Wallace <ken@beecavegames.com>
2016-01-31 02:42:54 -0600

    Setup the pin assets.

--- FToSleep/Assets/Scripts/Doors/DoorsPin.cs
+++ FToSleep/Assets/Scripts/Doors/DoorsPin.cs
@@ -4,9 +4,9 @@ using System.Collections;
 public class DoorsPin : MonoBehaviour {
 
 	private float _speed = 5.0f;
-	private float _upPos = 3.0f;
-	private float _bouncePos =  1.7f;
-	private float _downPos = 0.0f;
+	private float _upPos = 4.0f;
+	private float _bouncePos =  3.4f;
+	private float _downPos = 1.5f;
 	private Vector3 _currentPos;
 	private bool _bounce = false;
 
