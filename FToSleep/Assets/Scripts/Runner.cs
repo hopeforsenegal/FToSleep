0da08b9edace6c135f5641101054d839386d757d
Kamau Vassall <kamau.vassall@gmail.com>
2016-01-30 23:14:57 -0600

    Play audio through the animation

--- FToSleep/Assets/Scripts/Runner.cs
+++ FToSleep/Assets/Scripts/Runner.cs
@@ -6,12 +6,9 @@ using System.Collections.Generic;
 [RequireComponent (typeof(PlayerInput))]
 public class Runner : MonoBehaviour
 {
-	public AudioClip soundEffect;
-
 	public void Attack ()
 	{
 		Animator anim = GetComponentInParent<Animator> ();
 		anim.SetTrigger ("Swipe");
-		AudioController.PlaySoundEffect (soundEffect);
 	}
 }
