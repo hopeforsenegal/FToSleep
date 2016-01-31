063904f14b7657225a91a140f269154e314589e3
Ken Wallace <ken@beecavegames.com>
2016-01-30 23:26:52 -0600

    Added a dimming light effect over time.

---
+++ FToSleep/Assets/Scripts/Doors/DoorsOverlay.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Image = UnityEngine.UI.Image;
+using System.Collections;
+
+public class DoorsOverlay : MonoBehaviour {
+
+	public Image image;
+	public float speed = 1.0f;
+
+	private Color _color;
+
+	// Use this for initialization
+	void Start () {
+		_color = Color.black;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		image.color = Color.Lerp (image.color, _color, speed * Time.deltaTime);
+	}
+
+}
