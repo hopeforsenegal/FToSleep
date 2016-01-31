910de91480950e5587d11ac86f1c5a1a9d012213
Kamau Vassall <kamau.vassall@gmail.com>
2016-01-30 23:03:58 -0600

    Runner makes attack noise now

--- FToSleep/Assets/Scripts/Runner.cs
+++ FToSleep/Assets/Scripts/Runner.cs
@@ -6,9 +6,12 @@ using System.Collections.Generic;
 [RequireComponent (typeof(PlayerInput))]
 public class Runner : MonoBehaviour
 {
+	public AudioClip soundEffect;
+
 	public void Attack ()
 	{
 		Animator anim = GetComponentInParent<Animator> ();
 		anim.SetTrigger ("Swipe");
+		AudioController.PlaySoundEffect (soundEffect);
 	}
 }
